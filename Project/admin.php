a85a8a505805d08f30d8ad6ba09cbf4e8b035f73
jyothimp <30926794+jyothimp@users.noreply.github.com>
2017-11-18 09:43:13 +0530

    Test Commit

--- Project/admin.php
+++ Project/admin.php
@@ -2,77 +2,54 @@
 include_once 'db_connect.php';
 include_once 'check_logout.php';
 ?>
-
 <!doctype html>
 <html>
 <head>
-
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, maximum-scale=1, initial-scale=1, user-scalable=0">
 	<meta name="keywords" content="Destination Weddings in Kochi,Destination Weddings in Kerala,Destination Wedding in Kochi,Destination Wedding in Kerala,Destination Weddings">
 	<meta name="description" content="Scenario wedding company has the facilities of destination weddings. In the God's own country, there are a lot of beautiful locations to plan the destination wedding. Contact us : scenariowedding@gmail.com, +91 99464 90001">
 	<meta name="author" content="Invosome">
-
 	<title>Destination Weddings in Kochi Kerala | Scenario </title>
-
 	<!-- FAVICON AND APPLE TOUCH -->
 	<link rel="shortcut icon" href="images/icons/favicon.png">
 	<link rel="apple-touch-icon-precomposed" sizes="57x57" href="images/icons/apple-touch-57x57.png">
 	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/icons/apple-touch-72x72.png">
 	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/icons/apple-touch-114x114.png">
 	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/icons/apple-touch-144x144.png">
-
 	<!-- FONTS -->
 	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic">
-
 	<!-- BOOTSTRAP CSS -->
 	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
-
 	<!-- FONT AWESOME -->
 	<link rel="stylesheet" href="assets/fontawesome/css/font-awesome.min.css">
-
 	<!-- MIU ICON FONT -->
 	<link rel="stylesheet" href="assets/miuiconfont/miuiconfont.css">
-
 	<!-- FANCYBOX -->
 	<link rel="stylesheet" href="assets/js/fancybox/jquery.fancybox.css">
-
 	<!-- REVOLUTION SLIDER -->
 	<link rel="stylesheet" href="assets/js/revolutionslider/css/settings.css">
-
 	<!-- BxSLIDER -->
 	<link rel="stylesheet" href="assets/js/bxslider/jquery.bxslider.css">
-
 	<!-- YOUTUBE PLAYER -->
 	<link rel="stylesheet" href="assets/js/ytplayer/css/YTPlayer.css">
-
 	<!-- ANIMATIONS -->
 	<link rel="stylesheet" href="assets/js/animations/animate.min.css">
-
 	<!-- CUSTOM & PAGES STYLE -->
 	<link rel="stylesheet" href="assets/css/custom.css">
 	<link rel="stylesheet" href="assets/css/pages-style.css">
-
 	<!-- STYLE SWITCHER -->
 	<link rel="stylesheet" href="assets/js/style-switcher/style-switcher.css">
-
 	<!-- ALTERNATIVE STYLES -->
 	<link rel="stylesheet" href="#" data-style="styles">
-
-
 </head>
-
 <body>
-
-
 	<div id="page-wrapper">
-
 		<!-- HEADER -->
 		<header>
 			<div class="container">
 				<div class="row">
-
 					<div class="col-lg-6"></div>
 					<div class="col-lg-3" style="color:#ffaa00; text-align:right; font-size:14px;"><i class="fa fa-phone-square ph-icon" aria-hidden="true"></i> <a style="text-decoration:none;" href="mailto:angelwedding@gmail.com">angelwedding@gmail.com</a></div>
 					<div class="col-lg-2" style="color:#ffaa00; text-align:right; font-size:14px;"><i class="fa fa-envelope-o ph-icon" aria-hidden="true"></i> <a style="text-decoration:none;" href="tel:+919946490001">+91 99464 90001</a></div>
@@ -81,33 +58,23 @@ include_once 'check_logout.php';
 			<div class="container" style="border-bottom: 3px #110000">
 				<div class="row">
 					<div class="col-sm-2">
-
 						<!-- LOGO -->
 						<a id="logo" href="index.php">
 							<img src="images/icons/logo1.png" alt="Destination wedding in Kerala" width="165">
 						</a>
-
 					</div><!-- col -->
 					<div class="col-sm-10">
-
 						<!-- SEARCH -->
 						<!-- search-container -->
-
 						<!-- MENU -->
 						<nav>
-
 							<a id="mobile-menu-button" href="#"><i class="fa fa-bars"></i></a>
-
 							<ul class="menu clearfix" id="menu">
 								<li>
 									<a href="index.php">Home</a>
-
-
 								</li>
 								<li><a href="about-us.php">About</a></li>
-
 								<li class="megamenu ">
-
 									<div class="dropdown">
 										<button class="dropbtn">ITEMS</button>
 										<style>
@@ -119,12 +86,10 @@ include_once 'check_logout.php';
 											border: none;
 											cursor: pointer;
 										}
-
 										.dropdown {
 											position: relative;
 											display: inline-block;
 										}
-
 										.dropdown-content {
 											display: none;
 											position: absolute;
@@ -133,208 +98,124 @@ include_once 'check_logout.php';
 											box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
 											z-index: 1;
 										}
-
 										.dropdown-content a {
 											color: black;
 											padding: 12px 16px;
 											text-decoration: none;
 											display: block;
 										}
-
 										.dropdown-content a:hover {background-color: #f1f1f1}
-
 										.dropdown:hover .dropdown-content {
 											display: block;
 										}
-
 										.dropdown:hover .dropbtn {
 											background-color: white;
 										}
 										</style>
 									</head>
 									<body>
-
-
 										<div class="dropdown-content">
 											<a href="stage.php">Stages</a>
 											<a href="hall.php">Hall</a>
 											<a href="catering.php">Food</a>
 										</div>
 									</div>
-
-
-
-
-
 									<li >
 										<a href="contact-us.php">Contact</a>
-
 									</li>
 									<li>
 										<a href="logout.php">Logout</a>
-
 									</li>
-
 								</ul>
-
 							</nav>
-
 						</div><!-- col -->
 					</div><!-- row -->
 				</div><!-- container -->
-
 			</header><!-- HEADER -->
 			<style>
-
-
 			@media (min-width:992px) and (max-width:1199px){
 				.col-sm-10{
 					width: 100.333%;
 					margin-top: -4%;}
-
 				}
-
-
 				</style>
 				<!-- CONTENT -->
 				<div class="content">
-
 					<div id="page-header" class="dark-2">
 						<div class="container">
 							<div class="row">
 								<div class="col-sm-6">
-
 									<h4>Admin Page</h4>
-
-
 								</div><!-- col -->
-
 							</div><!-- row -->
 						</div><!-- ontainer -->
 					</div><!-- page-header -->
-
 					<div class="container">
 						<div class="row">
 							<div class="col-sm-12">
-
 								<!-- headline -->
-
 							</div><!-- col -->
 						</div><!-- row -->
 					</div><!-- container -->
-
 					<div class="container">
 						<div class="row">
 							<div class="col-sm-12">
-
 								<h2>Welcome Admin</h2>
-
 								<p>The ‘Destination Wedding’ in Kerala is an evolving concept. Across the world, foreign wedding travellers have been enchanted by the rich cultural heritage of Kerala. Kerala holds much to see and experience for the destination wedding traveller – starting from white sands and beautiful beaches to sparkling bay waters to high mountain ranges and other tropical habitats! Bestowed by its inherent beauty, Kerala is considered a natural choice for a destination wedding.</p>
-
 							</div>
-
 							<div class="col-sm-6">
 								<p>Destination wedding travellers seek Kerala to enjoy a peaceful getaway from the hustle and bustle of the fast-paced city life. Destination weddings are designed to give the guests an out-of-the-world experience. A destination wedding is a mix of a rejuvenating vacation and a wedding celebration. Often, destination weddings are held at cultural spots such as palaces and forts. Close family and kin are flown in from across the world for the destination wedding and are treated to a royal party, lasting a few days, in an exotic location.  </p>
 							</div>
-
 							<div class="col-sm-6">
 								<p>Destination weddings in Kerala occur mainly in the central districts of the state – Kochi, Alappuzha, Kottayam – to mention a few wedding destinations. Beach weddings, bay water weddings, palace weddings happen at many destinations in Kerala. We can make these destination weddings a lavish ceremony, clubbed with special incentives for sight-seeing in Kerala.</p>
 							</div>
 						</div><!-- col -->
 					</div><!-- row -->
 				</div><!-- container -->
-
 				<br><br>
-
-
-
-
-
 				<!-- container -->
-
-
-
-
-
-
 				<p>
 				</div><!-- portfolio-item-description -->
-
 			</div><!-- col -->
 		</div><!-- row -->
-
 	</div><!-- portfolio-item -->
-
-
-
-
 </div><!-- col -->
 </div><!-- row -->
 </div>
-
-
 </div><!-- CONTENT -->
-
-
 <!-- FOOTER -->
 <footer>
-
 	<div id="footer-top">
-
 		<!-- container -->
-
 	</div><!-- footer-top -->
-
 	<div id="footer">
-
 		<div class="container">
 			<div class="row">
 				<div class="col-sm-3">
-
 					<div class="widget widget-text">
-
 						<h3 class="widget-title">About Us</h3>
-
 						<p>As a trend setter We at , Angel have constantly shared the responsibility of our clients spreading the fragrance  and warmth  of hospitality all around.That brought us from a humble beginning of a wedding organising company to the present level of an  event management company of repute , over a period of seven years operating from the heart of Kerala, Kochi.</p>
-
 					</div><!-- widget-text -->
-
 					<div class="widget widget-newsletter">
-
 						<form name="newsletter" method="post" action="#">
 							<fieldset>
 								<input type="text" name="email" placeholder="Email address">
 								<input class="btn btn-default" type="submit" name="submit" value="">
 							</fieldset>
 						</form>
-
 						<p>Subscribe Now</p>
-
 					</div><!-- widget-newsletter -->
-
 				</div><!-- col -->
 				<div class="col-sm-3">
-
 					<div class="widget widget-latest-news">
-
 						<h3 class="widget-title">Reach Us</h3>
-
-
 						<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31417.266141615346!2d76.39011650000002!3d10.167787800000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b080625cfebb7d1%3A0x89bcd5a211b5e7e6!2sNedumbassery%2C+Kerala!5e0!3m2!1sen!2sin!4v1428733319906" width="250" height="350" frameborder="0" style="border:0"></iframe>
-
 					</div><!-- widget-recent-posts -->
-
 				</div><!-- col -->
 				<div class="col-sm-3">
 					<div class="widget widget-latest-news">
-
 						<h3 class="widget-title">Join With Us</h3>
-
 						<div id="fb-root"></div>
-
-
-
-
 						<script>(function(d, s, id) {
 							var js, fjs = d.getElementsByTagName(s)[0];
 							if (d.getElementById(id)) return;
@@ -342,44 +223,29 @@ include_once 'check_logout.php';
 							js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.0";
 							fjs.parentNode.insertBefore(js, fjs);
 						}(document, 'script', 'facebook-jssdk'));</script>
-
 						<div class="fb-like-box" data-href="https://www.facebook.com/pages/Scenario-Wedding-Company/558467804256076" data-width="250" data-height="360" data-colorscheme="dark" data-show-faces="true" data-header="false" data-stream="false" data-show-border="false"></div>
-
-
 					</div>
-
 					<div class="social-media abc">
-
-
 						<a class="twitter" href="https://twitter.com/scenariowc"><i class="fa fa-twitter"></i></a>
 						<a class="google" href="https://plus.google.com/109256524763879194099?hl=en"><i class="fa fa-google-plus"></i></a>
 						<a class="linkedin" href="https://www.linkedin.com/company/scenarioweddings?trk=biz-companies-cym"><i class="fa fa-linkedin"></i></a>
-
 					</div>
 				</div><!-- col -->
 				<div class="col-sm-3">
-
 					<div class="widget widget-contact">
-
 						<h3 class="widget-title">Contact Us</h3>
-
 						<ul>
 							<li>
-
-
-
 								<span>Address</span>
 								Priyadarshini Complex,<br>
 								Behind Hotel Airlink Castle.<br>
 								Athani, Nedumbassery P.O- 683 585.<br>
-
 							</li>
 							<li>
 								<span>Phone</span>
 								Ph: +91 8547855652<br>
 								+91  9946490001 <br>
 								+91  9946490003
-
 							</li>
 							<li>
 								<span>E-mail</span>
@@ -387,123 +253,80 @@ include_once 'check_logout.php';
 							</li>
 							<li>
 								<a href="faq.php"> <span>FAQ</span></a>
-
 							</li>
 						</ul>
-
 					</div><!-- widget-contact -->
-
 				</div><!-- col -->
 			</div><!-- row -->
 		</div><!-- container -->
-
 	</div><!-- footer -->
-
-
 	<div id="footer-bottom">
-
 		<div class="container">
 			<div class="row">
 				<div class="col-sm-12">
-
 					<div class="widget widget-text">
-
 						<p class="last text-center text-uppercase">Scenario Wedding Company &copy; All Rights Reserved
 							|
 							Designed By .:<a href="http://www.atees.in/" target="_blank">ATEES</a>:.
-
 						</div><!-- widget-text -->
-
 					</div><!-- col -->
 				</div><!-- row -->
 			</div><!-- container -->
-
 		</div><!-- footer-bottom -->
-
 	</footer><!-- FOOTER -->
-
-
 </div><!-- PAGE-WRAPPER -->
 <style>
 .abc
 {
 	padding-left:70px;
 	}</style><!-- FOOTER -->
-
 </div><!-- PAGE-WRAPPER -->
-
-
 <!-- GO TOP -->
 <a id="go-top"><i class="miu-icon-circle_arrow-up_glyph"></i></a>
-
-
 <!-- STYLE SWITCHER -->
 <div id="style-switcher"></div>
-
-
 <!-- jQUERY -->
 <script src="assets/js/jquery-2.1.3.min.js"></script>
-
 <!-- BOOTSTRAP JS -->
 <script src="assets/js/bootstrap.min.js"></script>
-
 <!-- VIEWPORT -->
 <script src="assets/js/viewport/jquery.viewport.js"></script>
-
 <!-- MENU -->
 <script src="assets/js/menu/hoverIntent.js"></script>
 <script src="assets/js/menu/superfish.js"></script>
-
 <!-- FANCYBOX -->
 <script src="assets/js/fancybox/jquery.fancybox.pack.js"></script>
-
 <!-- REVOLUTION SLIDER -->
 <script src="assets/js/revolutionslider/js/jquery.themepunch.tools.min.js"></script>
 <script src="assets/js/revolutionslider/js/jquery.themepunch.revolution.min.js"></script>
-
 <!-- BxSLIDER -->
 <script src="assets/js/bxslider/jquery.bxslider.min.js"></script>
-
 <!-- PARALLAX -->
 <script src="assets/js/parallax/jquery.parallax-scroll.min.js"></script>
-
 <!-- ISOTOPE -->
 <script src="assets/js/isotope/imagesloaded.pkgd.min.js"></script>
 <script src="assets/js/isotope/isotope.pkgd.min.js"></script>
-
 <!-- PLACEHOLDER -->
 <script src="assets/js/placeholders/jquery.placeholder.min.js"></script>
-
 <!-- CONTACT FORM VALIDATE & SUBMIT -->
 <script src="assets/js/validate/jquery.validate.min.js"></script>
 <script src="assets/js/submit/jquery.form.min.js"></script>
-
 <!-- GOOGLE MAPS -->
 <script src="http://maps.google.com/maps/api/js?sensor=false"></script>
 <script src="assets/js/googlemaps/jquery.gmap.min.js"></script>
-
 <!-- CHARTS -->
 <script src="assets/js/charts/chart.min.js"></script>
 <script src="assets/js/charts/jquery.easypiechart.min.js"></script>
-
 <!-- COUNTER -->
 <script src="assets/js/counter/jQuerySimpleCounter.js"></script>
-
 <!-- YOUTUBE PLAYER -->
 <script src="assets/js/ytplayer/jquery.mb.YTPlayer.js"></script>
-
 <!-- TWITTER -->
 <script src="assets/js/twitter/twitterfetcher.js"></script>
-
 <!-- ANIMATIONS -->
 <script src="assets/js/animations/wow.min.js"></script>
-
 <!-- CUSTOM JS -->
 <script src="assets/js/custom.js"></script>
-
 <!-- STYLE SWITCHER -->
-
 </body>
-
-
 </html>
